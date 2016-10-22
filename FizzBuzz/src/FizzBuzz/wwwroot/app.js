4ebf35ae04c3c28b51fbe068cf00f5990a498408
Donna Ryan <donna@nerdrose.com>
2016-10-22 00:28:10 -0700

    Getting my task notes down

--- FizzBuzz/src/FizzBuzz/wwwroot/app.js
+++ FizzBuzz/src/FizzBuzz/wwwroot/app.js
@@ -1,3 +1,6 @@
+// Task - make this more dynamic - change strings and divisors - add more of each to the function as well
+var strings = ["Fizz", "Buzz"];
+var divisors = [3, 7];
 function evaluateExpression(input, firstDivisor, secondDivisor, firstString, secondString) {
     if ((input % firstDivisor == 0) && (input % secondDivisor == 0)) {
         return firstString + secondString;
--- FizzBuzz/src/FizzBuzz/wwwroot/app.ts
+++ FizzBuzz/src/FizzBuzz/wwwroot/app.ts
@@ -1,4 +1,9 @@
-﻿function evaluateExpression(input, firstDivisor, secondDivisor, firstString, secondString) {
+﻿// Task - make this more dynamic - change strings and divisors - add more of each to the function as well
+
+let strings = [`Fizz`, `Buzz`];
+let divisors = [3, 7];
+
+function evaluateExpression(input, firstDivisor, secondDivisor, firstString, secondString) {
     if ((input % firstDivisor == 0) && (input % secondDivisor == 0)) {
         return firstString + secondString;
     }
